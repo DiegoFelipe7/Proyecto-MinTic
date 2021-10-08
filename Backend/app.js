4ab42281fa827880c7e792bd602f0bb481bc3271
Cristian23988 <cfbravo@unimayor.edu.co>
2021-10-08 16:14:18 -0500

    ventas
    ventas

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,21 @@
 var express = require("express");
 var mongoose = require("mongoose");
+const cors = require("cors");
 var app = express();
 
 const productsRoutes = require("./routes/products");
+const ventasRoutes = require("./routes/ventas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 //coneccion a la db mongo
 mongoose.connect("mongodb+srv://alejo:IIrpVJdHq1P7H9KE@cluster0.702jh.mongodb.net/improTicDB?retryWrites=true&w=majority").then(() => {
     console.log("Estamos conectados")
 });
 
 app.use("/api/products", productsRoutes);
+app.use("/api/ventas", ventasRoutes);
+
 
 module.exports = app;
