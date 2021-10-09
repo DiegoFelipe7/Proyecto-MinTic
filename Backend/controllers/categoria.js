6016c58348d5fdaf94453393f9938e5607f399ea
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2021-10-09 11:08:14 -0500

    cambios categoria

--- Backend/controllers/categoria.js
+++ Backend/controllers/categoria.js
@@ -2,10 +2,15 @@ const Categoria = require("../models/categoria");
 
 exports.CreateCategoria = (request, response) => {
   const categoriaForAdd = new Categoria({
-    nombre: request.body.nombre,
+    nombre_categoria: request.body.nombre_categoria,
   });
 
   categoriaForAdd.save().then((categoriaCreated) => {
     response.status(201).json(categoriaCreated);
   });
+};
+exports.getCategorias = (req, res) => {
+  Categoria.find().then((categorias) => {
+    res.status(200).json(categorias)
+  })
 };
